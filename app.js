fe2ceca62179c965eabf40f087c3a7e2cce2ad9e
yusufalp <alpyusuf007@gmail.com>
2024-09-09 18:43:29 -0700

    add routes to server and create error handler middleware

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-import express, { json, urlencoded } from "express";
+import "dotenv/config";
+import "./config/database.js";
+
+import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+
+import userRoutes from "./routes/userRoutes.js";
+import meetingRoutes from "./routes/meetingRoutes.js";
 
 const app = express();
 
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use(json());
-app.use(urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", userRoutes);
+app.use("/api/meetings", meetingRoutes)
 
 app.get("/", (req, res, next) => {
   res.send("We are just getting started...");
+});
+
+app.use((err, req, res, next) => {
+  console.error("An error occurred:", err);
+
+  if (err.code === 11000) {
+    return res.status(400).json({
+      error: { message: "There was a problem when signing up." },
+    });
+  }
+
+  return res.status(err.code || 500).json({
+    error: { message: err.message || "Internal server error." },
+  });
 });
 
 const PORT = 8080;
