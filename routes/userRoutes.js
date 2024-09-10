b5d93506c0601dfecd47f3d1eaa360b61e4b3f59
yusufalp <alpyusuf007@gmail.com>
2024-09-09 19:19:53 -0700

    add api endpoint to update user role

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,17 @@ import {
   loginUser,
   logoutUser,
   signupUser,
+  updateRole,
 } from "../controllers/userController.js";
+import { authorizeAdmin } from "../middlewares/authorizeAdmin.js";
+import { authenticateToken } from "../middlewares/authenticateToken.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+
+router.post("/update-role", authenticateToken, authorizeAdmin, updateRole);
 
 export default router;
