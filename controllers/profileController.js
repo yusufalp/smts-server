4d419034c9e740702b7355d61ba8dcd5b2cb1a0b
yusufalp <alpyusuf007@gmail.com>
2024-09-17 12:56:56 -0700

    include methods to update fields in the profile collection

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,19 +3,15 @@ import Profile from "../models/profileModel.js";
 export const getProfilesByUserId = async (req, res, next) => {
   const { userId } = req.params;
 
-  if (!userId) {
-    return res.status(400).json({
-      error: { message: "User id is required" },
-    });
-  }
-
   try {
+    if (!userId) {
+      throw new Error("User id is required");
+    }
+
     const profile = await Profile.find({ userId });
 
     if (!profile) {
-      return res.status(400).json({
-        error: { message: "Profile not found" },
-      });
+      throw new Error("Profile not found");
     }
 
     res.status(200).json({
@@ -27,8 +23,101 @@ export const getProfilesByUserId = async (req, res, next) => {
   }
 };
 
-export const updateEmail = async (req, res, next) => {};
+export const updateEmail = async (req, res, next) => {
+  const { email } = req.body;
 
-export const updateAddress = async (req, res, next) => {};
+  const userId = req.user._id;
 
-export const updateLinks = async (req, res, next) => {};
+  try {
+    if (!userId) {
+      throw new Error("User id is required");
+    }
+
+    if (!email) {
+      throw new Error("Email is required");
+    }
+
+    const profile = await Profile.findOneAndUpdate(
+      { userId },
+      { email },
+      { new: true }
+    );
+
+    if (!profile) {
+      throw new Error("Profile not found");
+    }
+
+    res.status(200).json({
+      success: { message: "Email updated successfully" },
+      data: { profile },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateAddress = async (req, res, next) => {
+  const { line1, line2, city, state, zip } = req.body;
+
+  const userId = req.user._id;
+
+  const address = {
+    street: {
+      line1,
+      line2,
+    },
+    city,
+    state,
+    zip,
+  };
+
+  try {
+    const profile = await Profile.findOneAndUpdate(
+      { userId },
+      { address },
+      { new: true }
+    );
+
+    if (!profile) {
+      throw new Error("Profile not found");
+    }
+
+    res.status(200).json({
+      success: { message: "Address updated successfully" },
+      data: { profile },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateLinks = async (req, res, next) => {
+  const { portfolio, linkedin, github } = req.body;
+
+  const userId = req.user._id;
+
+  const links = {
+    portfolio,
+    linkedin,
+    github,
+  };
+
+  try {
+    const profile = await Profile.findOneAndUpdate(
+      { userId },
+      { links },
+      { new: true }
+    );
+
+    if (!profile) {
+      throw new Error("Profile not found");
+    }
+
+    res.status(200).json({
+      success: { message: "Links updated successfully" },
+      data: { profile },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
