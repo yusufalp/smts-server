c42c3aa4beefc0637bdc0b8bc712299fff7a8277
yusufalp <alpyusuf007@gmail.com>
2024-09-29 13:31:43 -0700

    optimize queries

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,9 +1,63 @@
 import Profile from "../models/profileModel.js";
-
 import CustomError from "../utils/CustomError.js";
+
+export const getProfileByUserId = async (req, res, next) => {
+  const { userId } = req.params;
+
+  try {
+    if (!userId) {
+      throw new CustomError("User id is required", 400);
+    }
+
+    const profile = await Profile.findOne({ userId }).lean();
+
+    if (!profile) {
+      throw new CustomError("Profile not found", 404);
+    }
+
+    return res.status(200).json({
+      success: { message: "Profile found successfully" },
+      data: { profile },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProfilesByRole = async (req, res, next) => {
+  const { role, field = "" } = req.query;
+
+  try {
+    if (!role) {
+      throw new CustomError("Role is required", 400);
+    }
+
+    const roleConditions = {
+      advisor: { $or: [{ role: "mentor" }, { role: "coach" }] },
+      learner: { $or: [{ role: "mentee" }, { role: "alumni" }] },
+    };
+
+    const queryCondition = roleConditions[role] || { role };
+
+    const profiles = await Profile.find(queryCondition, field).lean();
+
+    if (!profiles.length) {
+      throw new CustomError(`Profiles with ${role} role not found`, 404);
+    }
+
+    return res.status(200).json({
+      success: { message: "Profiles by role found" },
+      data: { profiles },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getAssignedAdvisors = async (req, res, next) => {
   const userId = req.user._id;
+  // ? if no field is provided, is it undefined or empty string
+  // ? Do you want to assign a default value of name? Anything at all?
   const field = req.query.field || "name";
 
   try {
@@ -13,10 +67,11 @@ export const getAssignedAdvisors = async (req, res, next) => {
 
     const advisors = await Profile.findOne({ userId }, "assigned")
       .populate("assigned.mentor", field)
-      .populate("assigned.coach", field);
+      .populate("assigned.coach", field)
+      .lean();
 
     if (!advisors) {
-      throw new CustomError("Profile not found", 404);
+      throw new CustomError("Assigned advisors not found", 404);
     }
 
     return res.status(200).json({
@@ -48,7 +103,7 @@ export const getAssignedMentees = async (req, res, next) => {
         { "assigned.mentor": profile._id },
         { "assigned.coach": profile._id },
       ],
-    });
+    }).lean();
 
     if (!mentees) {
       throw new CustomError("Mentees not found", 404);
@@ -63,153 +118,53 @@ export const getAssignedMentees = async (req, res, next) => {
   }
 };
 
-export const getProfileByUserId = async (req, res, next) => {
-  const { userId } = req.params;
+export const updateProfileField = async (req, res, next) => {
+  const userId = req.user._id;
+
+  const { field, value } = req.body;
 
   try {
     if (!userId) {
       throw new CustomError("User id is required", 400);
     }
 
-    const profile = await Profile.findOne({ userId });
-
-    if (!profile) {
-      throw new CustomError("Profile not found", 400);
+    if (!field || !value) {
+      throw new CustomError(`${field} and its value is required`, 400);
     }
 
-    return res.status(200).json({
-      success: { message: "Profile found successfully" },
-      data: { profile },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+    const updateData = {};
 
-export const getProfilesByRole = async (req, res, next) => {
-  const { field = "", role } = req.query;
-
-  try {
-    if (!role) {
-      throw new CustomError("Role is required", 400);
-    }
-
-    let profiles;
-
-    if (role === "advisor") {
-      profiles = await Profile.find(
-        { $or: [{ role: "mentor" }, { role: "coach" }] },
-        field
-      );
+    if (field === "address") {
+      updateData[field] = {
+        street: {
+          line1: value.line1,
+          line2: value.line2,
+        },
+        city: value.city,
+        state: value.state,
+        zip: value.zip,
+        country: "US",
+      };
+    } else if (field === "links") {
+      updateData[field] = {
+        portfolio: value.portfolio,
+        linkedin: value.linkedin,
+        github: value.github,
+      };
     } else {
-      profiles = await Profile.find({ role }, field);
+      updateData[field] = value;
     }
 
-    if (!profiles) {
-      throw new CustomError("Profile by role not found", 404);
-    }
-
-    return res.status(200).json({
-      success: { message: "Profiles by role found" },
-      data: { profiles },
+    const profile = await Profile.findOneAndUpdate({ userId }, updateData, {
+      new: true,
     });
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const updateEmail = async (req, res, next) => {
-  const { email } = req.body;
-
-  const userId = req.user._id;
-
-  try {
-    if (!userId) {
-      throw new CustomError("User id is required", 400);
-    }
-
-    if (!email) {
-      throw new CustomError("Email is required", 400);
-    }
-
-    const profile = await Profile.findOneAndUpdate(
-      { userId },
-      { email },
-      { new: true }
-    );
 
     if (!profile) {
       throw new CustomError("Profile not found", 404);
     }
 
     return res.status(200).json({
-      success: { message: "Email updated successfully" },
-      data: { profile },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const updateAddress = async (req, res, next) => {
-  const { line1, line2, city, state, zip } = req.body;
-
-  const userId = req.user._id;
-
-  const address = {
-    street: {
-      line1,
-      line2,
-    },
-    city,
-    state,
-    zip,
-  };
-
-  try {
-    const profile = await Profile.findOneAndUpdate(
-      { userId },
-      { address },
-      { new: true }
-    );
-
-    if (!profile) {
-      throw new CustomError("Profile not found", 404);
-    }
-
-    return res.status(200).json({
-      success: { message: "Address updated successfully" },
-      data: { profile },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const updateLinks = async (req, res, next) => {
-  const { portfolio, linkedin, github } = req.body;
-
-  const userId = req.user._id;
-
-  const links = {
-    portfolio,
-    linkedin,
-    github,
-  };
-
-  try {
-    const profile = await Profile.findOneAndUpdate(
-      { userId },
-      { links },
-      { new: true }
-    );
-
-    if (!profile) {
-      throw new CustomError("Profile not found", 404);
-    }
-
-    return res.status(200).json({
-      success: { message: "Links updated successfully" },
+      success: { message: `${field} updated successfully` },
       data: { profile },
     });
   } catch (error) {
