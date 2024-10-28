737133ff2ffed89869387249f9ec041037de3829
yusufalp <alpyusuf007@gmail.com>
2024-10-27 17:48:41 -0700

    use plural names for files and enums

---
+++ enums/statuses.js
@@ -0,0 +1,13 @@
+// export const STATUSES = ["active", "inactive", "graduated"];
+
+export const STATUSES = Object.freeze({
+  active: { key: "active", desc: "" },
+  inactive: { key: "inactive", desc: "" },
+  graduated: { key: "graduated", desc: "" },
+});
+
+// export const STATUSES = new Map([
+//   ["active", { key: "active", desc: "" }],
+//   ["inactive", { key: "inactive", desc: "" }],
+//   ["graduated", { key: "graduated", desc: "" }],
+// ]);
