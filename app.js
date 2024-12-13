1d3c58efaecf128cfeaee98ff2c5cf2a4722a70d
yusufalp <alpyusuf007@gmail.com>
2024-12-12 21:22:41 -0800

    remove links to user and auth routes

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 
+import connectDB from "./config/database.js";
+
 import adminRoutes from "./routes/adminRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
 import meetingRoutes from "./routes/meetingRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
 
-import connectDB from "./config/database.js";
+import { authenticateToken } from "./middlewares/authenticateToken.js";
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use(helmet());
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
@@ -29,11 +29,11 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(authenticateToken);
+
 app.use("/api/admin", adminRoutes);
-app.use("/api/auth", authRoutes);
 app.use("/api/meetings", meetingRoutes);
 app.use("/api/profiles", profileRoutes);
-app.use("/api/users", userRoutes);
 
 app.get("/", (req, res, next) => {
   res.send("We are just getting started...");
@@ -56,5 +56,8 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`
+    Server is listening on port ${PORT}. 
+    http://localhost:${PORT}
+    `);
 });
