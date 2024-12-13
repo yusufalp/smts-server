6248eede34729f4715cdbe2ba4d83fc859e0eb76
yusufalp <alpyusuf007@gmail.com>
2024-12-12 21:25:28 -0800

    reorganize file and remove comments

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,30 +1,26 @@
 import express from "express";
 
-import { authenticateToken } from "../middlewares/authenticateToken.js";
 import {
+  createProfile,
   getAdvisors,
   getAssignedAdvisors,
-  getAssignedMentees,
   getAssignedMenteeById,
-  updateProfile,
+  getAssignedMentees,
   getProfileByUserId,
+  updateProfile,
 } from "../controllers/profileController.js";
-import { authorizeAdvisor } from "../middlewares/authorizeAdmin.js";
 
 const router = express.Router();
 
-// Protect all routes with token authentication
-router.use(authenticateToken);
+router.post("/profile", createProfile);
 
 router.get("/advisors", getAdvisors);
 router.get("/assigned/advisors", getAssignedAdvisors);
-router.get("/assigned/mentees", getAssignedMentees);
 router.get("/assigned/mentee/:_id", getAssignedMenteeById);
+router.get("/assigned/mentees", getAssignedMentees);
+
+router.get("/profile", getProfileByUserId);
 
 router.patch("/profile", updateProfile);
-
-// Only advisors (mentors/coaches) can access specific user profiles
-router.use(authorizeAdvisor);
-router.get("/:userId", getProfileByUserId);
 
 export default router;
