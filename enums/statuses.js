524f3931f4059570e0bfbfed1cb26f4aba9f9d6e
yusufalp <alpyusuf007@gmail.com>
2024-12-12 21:23:50 -0800

    remove comments

--- enums/statuses.js
+++ enums/statuses.js
@@ -1,13 +1,5 @@
-// export const STATUSES = ["active", "inactive", "graduated"];
-
 export const STATUSES = Object.freeze({
   active: { key: "active", desc: "" },
   inactive: { key: "inactive", desc: "" },
   graduated: { key: "graduated", desc: "" },
 });
-
-// export const STATUSES = new Map([
-//   ["active", { key: "active", desc: "" }],
-//   ["inactive", { key: "inactive", desc: "" }],
-//   ["graduated", { key: "graduated", desc: "" }],
-// ]);
