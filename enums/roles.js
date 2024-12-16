60b570f0bd4c8637e23a44ffc8f8ecf08d12bd6a
yusufalp <alpyusuf007@gmail.com>
2024-12-15 22:02:43 -0800

    add description to roles

--- enums/roles.js
+++ enums/roles.js
@@ -1,8 +1,8 @@
 export const ROLES = Object.freeze({
-  admin: { key: "admin" },
-  mentor: { key: "mentor" },
-  coach: { key: "coach" },
-  mentee: { key: "mentee" },
-  alumni: { key: "alumni" },
-  guest: { key: "guest" },
+  admin: { key: "admin", desc: "" },
+  mentor: { key: "mentor", desc: "" },
+  coach: { key: "coach", desc: "" },
+  mentee: { key: "mentee", desc: "" },
+  alumni: { key: "alumni", desc: "" },
+  guest: { key: "guest", desc: "" },
 });
