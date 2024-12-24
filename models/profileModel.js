e3762d778746ee2c27559f66ea66ee89ed0cdca4
yusufalp <alpyusuf007@gmail.com>
2024-12-24 12:45:26 -0800

    update profile schema

--- models/profileModel.js
+++ models/profileModel.js
@@ -4,15 +4,16 @@ const profileSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", unique: true },
     name: {
-      first: {
+      firstName: {
         type: String,
         required: true,
       },
-      last: {
+      lastName: {
         type: String,
       },
     },
     email: { type: String, unique: true, required: true },
+    phoneNumber: { type: Number },
     address: {
       street: {
         line1: { type: String },
@@ -20,24 +21,25 @@ const profileSchema = new Schema(
       },
       city: { type: String },
       state: { type: String },
-      zip: { type: Number },
+      postalCode: { type: Number },
       country: { type: String },
     },
     links: {
-      portfolio: { type: String },
-      linkedin: { type: String },
-      github: { type: String },
+      portfolioUrl: { type: String },
+      linkedinUrl: { type: String },
+      githubUrl: { type: String },
     },
-    assigned: {
-      mentor: { type: Schema.Types.ObjectId, ref: "Profile", default: null },
-      coach: { type: Schema.Types.ObjectId, ref: "Profile", default: null },
+    bio: { type: String },
+    assignedRoles: {
+      mentorId: { type: Schema.Types.ObjectId, ref: "Profile" },
+      coachId: { type: Schema.Types.ObjectId, ref: "Profile" },
     },
     // * active, inactive, graduated
-    status: { type: String, default: "active" },
+    accountStatus: { type: String, default: "active" },
     // * admin, mentor, coach, mentee, alumni, guest
     role: { type: String, default: "mentee" },
     cohort: { type: Number },
-    graduation: { type: Date },
+    graduationDate: { type: Date },
   },
   { timestamps: true }
 );
