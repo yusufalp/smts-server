f48b494ddda41c4994dd9907575cd1cb4961305b
yusufalp <alpyusuf007@gmail.com>
2024-12-24 21:37:15 -0800

    refactor methods

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,26 +2,33 @@ import express from "express";
 
 import {
   createProfile,
+  getAllAdvisors,
   getAssignedAdvisors,
+  getAssignedLearnerById,
   getAssignedLearners,
-  updateProfile,
   getProfile,
-  getAssignedMenteesByMenteeId,
+  updateProfile,
 } from "../controllers/profileController.js";
 
 const router = express.Router();
+
+// POST /api/profiles/profile
+// Creates a new profile.
+router.post("/profile", createProfile);
+
+// GET /api/profiles/profile
+// Retrieve a list of all mentors and coaches
+router.get("/advisors", getAllAdvisors);
 
 // GET /api/profiles/assigned/advisors
 // Retrieves a list of own advisors
 router.get("/assigned/advisors", getAssignedAdvisors);
 
+router.get("/assigned/learner/:_id", getAssignedLearnerById);
+
 // GET /api/profiles/assigned/learners
 // Retrieves a list of own learners
 router.get("/assigned/learners", getAssignedLearners);
-
-// POST /api/profiles/profile
-// Creates a new profile.
-router.post("/profile", createProfile);
 
 // GET /api/profiles/profile
 // Retrieves own profile.
@@ -31,5 +38,4 @@ router.get("/profile", getProfile);
 // Updates own profile.
 router.patch("/profile", updateProfile);
 
-router.get("/assigned/mentees/:menteeId", getAssignedMenteesByMenteeId);
 export default router;
